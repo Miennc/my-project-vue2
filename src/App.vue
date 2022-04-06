d9b310be71921427909dfe466a5afd0784d8bb19
Mien <2502mien@gmail.com>
2022-04-06 09:02:57 +0700

    'add

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
-    <div>
-        <ChildComponent :msg="getting" @conchochatien="chanhantien"/>
-    </div>
+ <div>
+   <MyButton> </MyButton>
+ </div>
 </template>
-
-
 <script>
-import ChildComponent from './components/ChildComponent.vue'
 export default {
-  data() {
-      return {
-          getting: 'msg from component father'
-      }
-  },    
-  components: {
-      ChildComponent
-  },
-  methods: {
-      chanhantien(money){
-        alert(money); 
-        alert('thank you so much')     
-      }
-  },
+ data() {
+     return {
+
+     }
+ },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 import index from './index.css'
+
+Vue.component('MyButton', {
+  data() {
+    return {
+      buttonText:'myButton'
+    }
+  },
+  template: `<button>   {{buttonText}}</button>`
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
